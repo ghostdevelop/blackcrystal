bab528fe174eb098aa16c2e13566208ff795a3b6
ghostdevelop <ghost46@protonmail.com>
2016-11-26 00:24:02 -0500

    last today

--- woocommerce/myaccount/my-address.php
+++ woocommerce/myaccount/my-address.php
@@ -22,7 +22,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 $customer_id = get_current_user_id();
 
 if ( ! wc_ship_to_billing_address_only() && get_option( 'woocommerce_calc_shipping' ) !== 'no' ) {
-	$page_title = apply_filters( 'woocommerce_my_account_my_address_title', __( 'My Addresses', 'woocommerce' ) );
+	$page_title = apply_filters( 'woocommerce_my_account_my_address_title', __( 'My Addresses', 'blackcrystal' ) );
 	$get_addresses    = apply_filters( 'woocommerce_my_account_get_addresses', array(
 		'billing' => __( 'Billing Address', 'woocommerce' ),
 		'shipping' => __( 'Shipping Address', 'woocommerce' )
