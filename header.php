488b6e847431c7477a8a8d0d2aed543909485a00
ghostdevelop <ghost46@protonmail.com>
2017-01-18 18:11:01 -0500

    callback hungary only

--- header.php
+++ header.php
@@ -132,10 +132,12 @@
 				<div class="clear"></div>
 			</div>
 		</div>
-		<?php if (SIMPLE_SHOP):?>
-			<?php get_template_part('header-simple')?>
-		<?php else:?>
-			<?php get_template_part('header-wholesale')?>
+		<?php if (get_locale() == 'hu_HU'):?>
+			<?php if (SIMPLE_SHOP):?>
+				<?php get_template_part('header-simple')?>
+			<?php else:?>
+				<?php get_template_part('header-wholesale')?>
+			<?php endif;?>
 		<?php endif;?>
 		<div class="nav-container">
 			<div class="container">
