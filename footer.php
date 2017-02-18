fc8b9ce2577148b002bbfb7a75bd2e0ebbdf74a1
ghostdevelop <ghost46@protonmail.com>
2017-02-18 11:51:10 -0500

    payment logos

--- footer.php
+++ footer.php
@@ -23,6 +23,19 @@
 					</div>
 				</div>
 				<?php if (SIMPLE_SHOP):?>
+
+					<ul class="accept-cards">
+						<li><img src="<?php echo get_template_directory_uri()?>/images/payment-logos/paypal.png" /></li>
+						<li><img src="<?php echo get_template_directory_uri()?>/images/payment-logos/maestro.gif" /></li>
+						<li><img src="<?php echo get_template_directory_uri()?>/images/payment-logos/master-card.gif" /></li>
+						<li><img src="<?php echo get_template_directory_uri()?>/images/payment-logos/american-express.jpeg" /></li>
+						<li><img src="<?php echo get_template_directory_uri()?>/images/payment-logos/jcb.png" /></li>
+						<li><img src="<?php echo get_template_directory_uri()?>/images/payment-logos/visa-electron-new-vector-logo.png" /></li>
+						<li><img src="<?php echo get_template_directory_uri()?>/images/payment-logos/visa-logo.png" /></li>
+						<li><img src="<?php echo get_template_directory_uri()?>/images/payment-logos/vpay-logo" /></li>
+					</ul>
+				<?php endif; ?>
+				<?php if (SIMPLE_SHOP):?>
 					<img src="<?php echo get_option('kh_logo')?>" class="khlogo"/>
 				<?php endif;?>
 			</div>
